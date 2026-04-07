ee94e079aee750bac589e782c61ed08e87a4e394
Jamil Jones <jamiljones@mac.com>
2026-04-07 10:01:10 -0700

    Initial Commit

---
+++ JetSetter Pro/JetSetter_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetSetter_ProApp.swift
+//  JetSetter Pro
+//
+//  Created by Jamil Jones on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct JetSetter_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
